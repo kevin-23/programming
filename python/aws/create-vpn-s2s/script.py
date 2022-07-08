178e40301cdf28348bf836d839aca3e4d75c3f9c
Kevin Cabezas <kevin.23cabezas@gmail.com>
2022-07-07 23:39:43 -0500

    Function to create VPN S2S with a Static Route

--- python/aws/create-vpn-s2s/script.py
+++ python/aws/create-vpn-s2s/script.py
@@ -1,12 +1,18 @@
 import boto3
+import time
 
 # Enter parameters
-ipCGW = input(
-    str('~~~ Customer Gataway parameters ~~~\nEnter Customer Gateway IPv4: '))
-nameCGW = input(str('Enter Customer Gateway Name: '))
+nameCGW = input(
+    str('~~~ Customer Gataway parameters ~~~\nEnter Customer Gateway Name: '))
+ipCGW = input(str('Enter Customer Gateway IPv4: '))
+
+nameVPN = input(str('\n~~~ VPN S2S parameters ~~~\nEnter VPN Name: '))
+staticRoutes = input(str(
+    'Enter a Static Route IPv4. The CIDR block associated with the local subnet of the customer network: '))
 
 # Keys variables
 customerGWId = ''
+vpnId = ''
 
 # This function creates a customer gateway
 def cgw(ipCGW, nameCGW):
@@ -34,17 +40,47 @@ def cgw(ipCGW, nameCGW):
     )
 
     customerGWId = response['CustomerGateway']['CustomerGatewayId']
+    print('\nCreating Customer Gateway... Please wait.')
+    time.sleep(15)
 
-'''
-def vpnConnection(nameCGW)
+    vpnConnection(nameVPN, staticRoutes)
+
+# This function creates a VPN S2S
+def vpnConnection(nameVPN, staticRoutes):
+
+    global vpnId
+
+    client = boto3.client('ec2')
+
+    # VPN S2S Options
     response = client.create_vpn_connection(
-        CustomerGatewayId=nameCGW,
-        Type='string',
+        CustomerGatewayId=customerGWId,
+        Type='ipsec.1',
         VpnGatewayId='vgw-00f29de9944746ae3',
         Options={
             'StaticRoutesOnly': True,
-        }
+        },
+        TagSpecifications=[
+            {
+                'ResourceType': 'vpn-connection',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': nameVPN
+                    }
+                ]
+            }
+        ]
     )
-'''
+    vpnId = response['VpnConnection']['VpnConnectionId']
+    print('Creating VPN S2S... Please wait.')
+    time.sleep(15)
+
+    # Add static route
+    client.create_vpn_connection_route(
+        DestinationCidrBlock=staticRoutes,
+        VpnConnectionId=vpnId
+    )
+
 
 cgw(ipCGW, nameCGW)

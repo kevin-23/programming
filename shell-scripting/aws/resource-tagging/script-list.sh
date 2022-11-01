ce4e039457d24ecb0b2db0b4a344e2e753575c97
Kevin Cabezas <kevin.23cabezas@gmail.com>
2022-10-31 20:49:17 -0500

    Comments were added

--- shell-scripting/aws/resource-tagging/script-list.sh
+++ shell-scripting/aws/resource-tagging/script-list.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+# Reads the arn resources
 input="./arns.txt"
+
+# Counter variable
 counter=0
+
+# Loop that reads the arns.txt
 while read -r line
 do
 	
+	# Consult the resource tags
 	command=$(aws resourcegroupstaggingapi get-resources --resource-arn "$line")
 	let counter++
 	echo -e "$counter. $command $line\n"
 
+# Ends loop
 done < "$input"
--- shell-scripting/aws/resource-tagging/script-tag.sh
+++ shell-scripting/aws/resource-tagging/script-tag.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+# Reads the arn resources
 input="./arns.txt"
+
+# Counter variable
 counter=0
+
+# Loops that reads the arns.txt
 while read -r line
 do
 	
+	# Tags the resource	
 	command=$(aws resourcegroupstaggingapi tag-resources --resource-arn "$line" --tags environment=dev)
 	let counter++
 	echo -e "$counter. $command $line\n"
 
+# Ends loop
 done < "$input"

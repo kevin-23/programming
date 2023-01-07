bdcce55d369d76388cd460b1018b33a0fb584727
Kevin Cabezas <kevin.23cabezas@gmail.com>
2023-01-06 23:30:11 -0500

    added semicolon

--- nodejs/declarations/variables.js
+++ nodejs/declarations/variables.js
@@ -6,19 +6,19 @@
 
 let name = "Kevin"; // This variable can not be redeclared but its value can change
 let number;
-console.log(name)
+console.log(name);
 
 number = 10;
 name = "Luis";
 
-console.log(name, number)
+console.log(name, number);
 
 // The const keyword was also added in ES6. The difference with let variables
 // is that they can not change their value.
 //
 
 const PI = 3.14;
-console.log(`The PI value is: ${PI}`)
+console.log(`The PI value is: ${PI}`);
 
 //PI = 3.141; it will be error
 //
